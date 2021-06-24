f53abe1302a26490e936ac9a1f0097d303b973dc
Random <random.x@binance.com>
2021-06-24 17:41:13 +0800

    feat: add AWS

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and deploy with AWS
         </a>
       </header>
     </div>
